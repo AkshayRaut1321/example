a95be5646ba5d50d68c34318a2007d2c98e30ba6
Akshay Raut <rakshay08@gmail.com>
2019-04-09 01:45:01 +0530

    Http module

---
+++ src/app/new-course-form/new-course-form.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'new-course-form',
+  templateUrl: './new-course-form.component.html',
+  styleUrls: ['./new-course-form.component.css']
+})
+export class NewCourseFormComponent {
+  form = new FormGroup({
+    topics: new FormArray([])
+  });
+
+  addTopic(topic: HTMLInputElement) {
+    this.topics.push(new FormControl(topic.value));
+    topic.value = "";
+  }
+
+  get topics() {
+    return this.form.get('topics') as FormArray;
+  }
+
+  removeTopic(topic: FormControl){
+    let index = this.topics.controls.indexOf(topic);
+    this.topics.removeAt(index);
+  }
+
+  // constructor(fb: FormBuilder){
+  //   fb.group({
+  //     name:['', Validators.required],
+  //     contact: fb.group({
+  //       email:[],
+  //       phone: []
+  //     }),
+  //     topics: fb.array([])
+  //   })
+  // }
+}
